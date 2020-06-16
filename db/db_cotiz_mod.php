9e8131b6824eb886d0efe0daae2eedff9638abc0
Francisco Figueroa <panchosama@gmail.com>
2020-06-15 23:34:05 -0400

    Update db_cotiz_mod.php

--- db/db_cotiz_mod.php
+++ db/db_cotiz_mod.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
  
 //Preparamos la conexión con la base de datos
-    $db = new mysqli('localhost', 'sistema', 'sistheli09','helicopters_intra');
+    $db = new mysqli('server', 'usuario', 'pass','db');
  
 //Recepción de los datos
     $cotiz = json_decode(file_get_contents("php://input"));
